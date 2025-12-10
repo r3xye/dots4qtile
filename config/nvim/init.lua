2260921c05aaf4694c3da51f53a6d4c2ba2887d8
r3xye <minoi9876@gmail.com>
2025-12-10 16:49:41 +0100

    add nvim

---
+++ config/nvim/init.lua
@@ -0,0 +1,3 @@
+require("setting")
+require("binds") 
+require("config.lazy")
---
+++ config/nvim/lua/binds.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = " "
+
+vim.api.nvim_set_keymap('n', '<leader>e', ':Explore <CR>', {noremap = true,  silent = true})
+
+
