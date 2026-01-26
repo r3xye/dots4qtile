2914157a20380c65033224206b8de161a9de0e8f
r3xye <minoi9876@gmail.com>
2026-01-26 09:12:20 +0100

    rofi keybid

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -76,6 +76,7 @@ keys = [
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([mod], "e", lazy.spawn("rofi -show drun"), desc="spawn rofi"),
 ]
 
 # VTs for Wayland
